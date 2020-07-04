60c9c9b0d08d71df1c6731f480d5508242420c6c
Mohan raji <Mohan raji@DESKTOP-0UJ338V>
2020-07-04 18:50:51 +0530

    helo

---
+++ javaprograms/src/practice/simplepattern.java
@@ -0,0 +1,20 @@
+package practice;
+
+public class simplepattern 
+{
+
+	public static void main(String[] s)
+	{
+		simple1 b2=new simple1();
+		b2.display();
+	}}
+	class simple1 {
+		protected void display()
+		{
+			System.out.println("mohan");
+		}
+	}
+	
+
+	
+
